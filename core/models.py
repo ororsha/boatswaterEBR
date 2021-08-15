6b3a1e64106d1ecb5107db7367189f98b2810d60
Itsik-revivo <revivo516@gmail.com>
2021-08-15 21:03:34 +0300

    add date to product page

--- core/models.py
+++ core/models.py
@@ -46,7 +46,6 @@ class Item(models.Model):
     description = models.TextField()
     image = models.ImageField()
     image_2 = models.ImageField()
-    invited_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
@@ -77,6 +76,7 @@ class OrderItem(models.Model):
     ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
+    invited_date = models.DateTimeField()
 
 
     def __str__(self):
